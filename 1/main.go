a54e5554318cc07e3f771641283a88d006eb8b4c
Marc-Antoine Lafrenière <lafmarc18@gmail.com>
2018-12-02 15:34:30 -0500

    day 1

--- 1/main.go
+++ 1/main.go
@@ -13,12 +13,10 @@ func main() {
 	inputs := "L1, R3, R1, L5, L2, L5, R4, L2, R2, R2, L2, R1, L5, R3, L4, L1, L2, R3, R5, L2, R5, L1, R2, L5, R4, R2, R2, L1, L1, R1, L3, L1, R1, L3, R5, R3, R3, L4, R4, L2, L4, R1, R1, L193, R2, L1, R54, R1, L1, R71, L4, R3, R191, R3, R2, L4, R3, R2, L2, L4, L5, R4, R1, L2, L2, L3, L2, L1, R4, R1, R5, R3, L5, R3, R4, L2, R3, L1, L3, L3, L5, L1, L3, L3, L1, R3, L3, L2, R1, L3, L1, R5, R4, R3, R2, R3, L1, L2, R4, L3, R1, L1, L1, R5, R2, R4, R5, L1, L1, R1, L2, L4, R3, L1, L3, R5, R4, R3, R3, L2, R2, L1, R4, R2, L3, L4, L2, R2, R2, L4, R3, R5, L2, R2, R4, R5, L2, L3, L2, R5, L4, L2, R3, L5, R2, L1, R1, R3, R3, L5, L2, L2, R5"
 
 	pos := puzzle1(inputs)
-
-	fmt.Printf("Distance: %v\n", math.Abs(pos.X)+math.Abs(pos.Y))
+	fmt.Printf("Puzzle 1: %v\n", math.Abs(pos.X)+math.Abs(pos.Y))
 
 	pos = puzzle2(inputs)
-
-	fmt.Printf("Distance: %v\n", math.Abs(pos.X)+math.Abs(pos.Y))
+	fmt.Printf("Puzzle 2: %v\n", math.Abs(pos.X)+math.Abs(pos.Y))
 }
 
 func puzzle1(inputs string) r3.Vector {
@@ -43,7 +41,6 @@ func puzzle2(inputs string) r3.Vector {
 			pos = pos.Add(dir)
 
 			_, exists := visited[pos.String()]
-
 			if exists {
 				return pos
 			} else {
@@ -71,7 +68,7 @@ func rotate90(v r3.Vector, dir byte) r3.Vector {
 		return r3.Vector{X: v.Y, Y: -v.X}
 	case 'L':
 		return r3.Vector{X: -v.Y, Y: v.X}
+	default:
+		panic(fmt.Errorf("invalid rotation direction %q", dir))
 	}
-
-	panic(fmt.Errorf("invalid rotation direction %q", dir))
 }
